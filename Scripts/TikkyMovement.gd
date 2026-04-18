d8888818e1e8b9d899cb216a5d70106c7ca58dc9
kakoytoAndy <117573077+kakoytoAndy@users.noreply.github.com>
2026-04-19 01:04:15 +0300

    move thyself

--- Scripts/TikkyMovement.gd
+++ Scripts/TikkyMovement.gd
@@ -9,7 +9,24 @@ extends CharacterBody2D
 
 
 #АНДРЕЙ, ПОЖАЙЛУСТА НАПИШИ ТУТ КОД ДЛЯ ПЕРЕДВИЖЕНЯ, ХОТЯ БЫ САМЫЙ ПРОСТОЙ - фончи
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JumpHeight
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * WalkSpeed
+	else:
+		velocity.x = move_toward(velocity.x, 0, WalkSpeed)
+
+	move_and_slide()
 
 
 func camera_shake(Intensity: int, ZoomSpeed: float):
